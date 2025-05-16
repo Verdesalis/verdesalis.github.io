0328bc693aa18aeeb403d38f20afd72113baae48
Ivano Manca <ivano.manca@gmail.com>
2025-05-16 16:42:15 +0200

    Site updated at 2025-05-16 14:42:15 UTC

---
+++ js/main.js
@@ -0,0 +1,365 @@
+var normal = document.getElementById("nav-menu");
+var reverse = document.getElementById("nav-menu-left");
+
+var icon = normal !== null ? normal : reverse;
+
+// Toggle the "menu-open" % "menu-opn-left" classes
+function toggle() {
+    var navRight = document.getElementById("nav");
+    var navLeft = document.getElementById("nav-left");
+    var nav = navRight !== null ? navRight : navLeft;
+
+    var button = document.getElementById("menu");
+    var site = document.getElementById("wrap");
+
+    if (nav.className == "menu-open" || nav.className == "menu-open-left") {
+        nav.className = "";
+        button.className = "";
+        site.className = "";
+    } else if (reverse !== null) {
+        nav.className += "menu-open-left";
+        button.className += "btn-close";
+        site.className += "fixed";
+    } else {
+        nav.className += "menu-open";
+        button.className += "btn-close";
+        site.className += "fixed";
+    }
+}
+
+// Ensures backward compatibility with IE old versions
+function menuClick() {
+    if (document.addEventListener && icon !== null) {
+        icon.addEventListener('click', toggle);
+    } else if (document.attachEvent && icon !== null) {
+        icon.attachEvent('onclick', toggle);
+    } else {
+        return;
+    }
+}
+
+menuClick();
+
+function passwdCorsoRvatt1() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2017") {
+        window.open("https://jmp.sh/r1u9bIr")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt2() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2017") {
+        window.open("https://jmp.sh/UwlQLFW")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt3() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2017") {
+        window.open("https://jmp.sh/56lsxXc")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt4() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2017") {
+        window.open("https://jmp.sh/B8ZJ4tG")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt5() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2017") {
+        window.open("https://jmp.sh/nO4QiuD")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt6() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2017") {
+        window.open("https://jmp.sh/yBQYhDx")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt7() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2017") {
+        window.open("https://jmp.sh/59CeJrk")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt8() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2017") {
+        window.open("https://jmp.sh/K7FQSuc")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt9() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2018") {
+        window.open("https://jmp.sh/7x0Hf4H")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt10() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2018") {
+        window.open("https://jmp.sh/RJkO97o")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt11() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2018") {
+        window.open("https://jmp.sh/ERRRiaR")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvattem() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2018") {
+        window.open("https://jmp.sh/Ijs7bqP")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt12() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2018") {
+        window.open("https://jmp.sh/U7zwbFT")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt13() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2018") {
+        window.open("https://jmp.sh/SyYbAy3")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt14() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvatt2018") {
+        window.open("https://jmp.sh/o492IK1")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt15() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "canapa2018") {
+        window.open("https://jmp.sh/UzgcAIA")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt16() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "canapa2018") {
+        window.open("https://jmp.sh/twl8D6w")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt17() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "canapa2018") {
+        window.open("https://jmp.sh/xh3MzWx")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCorsoRvatt18() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "rvattlab2018") {
+        window.open("https://jmp.sh/ozWdrTn")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdLabZeo20() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "lz2020") {
+        window.open("https://jmp.sh/KQOq5Xo")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCIFZ23() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "cif52023") {
+        window.open("https://jmp.sh/SzJqoFUX")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdCIFS23() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "cif52023") {
+        window.open("https://jmp.sh/Qh1hf6V2")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdGDS124() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "gds2024") {
+        window.open("https://jmp.sh/wSrqTI87")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdGDS224() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "gds2024") {
+        window.open("https://jmp.sh/v2Sf2lE7")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdGDS324() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "gds2024") {
+        window.open("https://jmp.sh/PRyuIRkR")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdGDS424() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "gds2024") {
+        window.open("https://jmp.sh/PEvsdgHx")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdFF124() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "ffem2024") {
+        window.open("https://jmp.sh/BlUG2Prs")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdFF224() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "ffem2024") {
+        window.open("https://jmp.sh/S7JOOsLI")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdFF324() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "ffem2024") {
+        window.open("https://jmp.sh/acSp0dDj")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdLIF124() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "lif2024") {
+        window.open("https://jmp.sh/ocDVU5ol")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdLIF224() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "lif2024") {
+        window.open("https://jmp.sh/9uu2nlTA")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdFN124() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "fn2024") {
+        window.open("https://jmp.sh/yXzX8RKO")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdFN224() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "fn2024") {
+        window.open("https://jmp.sh/nFqPzlkX")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdFDS124() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "fds2024") {
+        window.open("https://jmp.sh/Terz0XbG")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdFDS224() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "fds2024") {
+        window.open("https://jmp.sh/lwOqQPA7")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
+
+function passwdFDS324() {
+    var password = prompt('Inserisci la password per scaricare il materiale del corso:');
+    if (password.toLowerCase() == "fds2024") {
+        window.open("https://jmp.sh/DXjzhFk0")
+    } else {
+        alert("La password non è corretta, riprova.");
+    }
+}
